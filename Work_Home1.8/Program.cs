b11c8d8ac7aa37ed8fea9421dd38acaf9dd3b80c
Maksim <bagzmax75@gmail.com>
2022-06-30 01:56:05 +0300

    add Work Home 1.8

---
+++ Work_Home1.8/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая на вход принимает число (N),
+// а на выходе показывает все чётные числа от 1 до N.
+
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+
+int number = 1;
+
+while (number <= N)
+{
+    if (number % 2 == 0)
+{
+    System.Console.WriteLine(number);
+}
+  number++;
+}
+
