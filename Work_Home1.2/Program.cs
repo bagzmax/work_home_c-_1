667eb87a187e85dd1a9166c028effa732114a177
Maksim <bagzmax75@gmail.com>
2022-06-30 15:26:59 +0300

    rename  Work Home 1.2

--- Work_Home1.2/Program.cs
+++ Work_Home1.2/Program.cs
@@ -1,4 +1,4 @@
-﻿//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+﻿//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 
 Console.Write("Введите первое число: ");
 int numberA = int.Parse(Console.ReadLine());
@@ -6,13 +6,23 @@ int numberA = int.Parse(Console.ReadLine());
 Console.Write("Введите второе число: ");
 int numberB = int.Parse(Console.ReadLine());
 
-Console.Write("Введите третье число: ");
-int numberC = int.Parse(Console.ReadLine());
-
 int max = numberA;
+int min = numberB;
 
-if (numberA > max) max = numberA;
-if (numberB > max) max = numberB;
-if (numberC > max) max = numberC;
+if (numberA == numberB)
+{
+    System.Console.WriteLine("Равные числа");
+}
+else { if (numberA > numberB)
+{
+    System.Console.WriteLine("max= " + max);
+    System.Console.Write("min= " + min);
+}
+else
+{
+   System.Console.WriteLine("max= " + min);
+   System.Console.Write("min= " + max);
+}
+}
 
-System.Console.WriteLine("Максимальное число = " + max);
+ 
