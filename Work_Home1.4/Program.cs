2428e9c416f5a997f58a4e574da7d09995a3ab26
Maksim <bagzmax75@gmail.com>
2022-06-30 15:27:26 +0300

    rename  Work Home 1.4

---
+++ Work_Home1.4/Program.cs
@@ -0,0 +1,18 @@
+﻿//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.Write("Введите первое число: ");
+int numberA = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int numberB = int.Parse(Console.ReadLine());
+
+Console.Write("Введите третье число: ");
+int numberC = int.Parse(Console.ReadLine());
+
+int max = numberA;
+
+if (numberA > max) max = numberA;
+if (numberB > max) max = numberB;
+if (numberC > max) max = numberC;
+
+System.Console.WriteLine("Максимальное число = " + max);
